9a7ba1a7f41e89363ba24485cc531078450506e9
Leonardo Lima <leonardotexeiralima@gmail.com>
2019-04-09 05:48:04 -0300

    Rule and lambda association

--- src/functions/rule/create.py
+++ src/functions/rule/create.py
@@ -10,12 +10,22 @@ aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
 
 
 def _rule_create(_event, _context):
-    client = boto3.client('events', aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
-    response = client.put_rule(
-        Name='test3',
-        ScheduleExpression='rate(5 minutes)',
+    rule_name = "test"
+    cloudwatch_events = boto3.client('events', aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
+    put_rule_response = cloudwatch_events.put_rule(
+        Name=rule_name,
+        ScheduleExpression='rate(30 minutes)',
         State='ENABLED'
     )
+    put_targets_response = cloudwatch_events.put_targets(
+        Rule=rule_name,
+        Targets=[
+            {
+                'Arn': 'arn:aws:lambda:us-east-1:653057332392:function:dev-queuePush',
+                'Id': 'myCloudWatchEventsTarget',
+            },
+        ]
+    )
     return {'statusCode': 201,
-            'body': json.dumps(response)}
+            'body': json.dumps(put_targets_response)}
 
